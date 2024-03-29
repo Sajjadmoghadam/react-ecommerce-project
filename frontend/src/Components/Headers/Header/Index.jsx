af73ebecf4ea68aa5bf575ddcb3cd69a3f7ddf91
Sajjadmoghadam <sajjad.moghadam1379@gmail.com>
2024-03-29 22:13:00 +0330

    create home first section

--- frontend/src/Components/Headers/Header/Index.jsx
+++ frontend/src/Components/Headers/Header/Index.jsx
@@ -7,8 +7,8 @@ const Header = () => {
     return (
         <>
 
-            <Stack direction={'row-reverse'} padding={'12px'} sx={{ backgroundColor: 'transparent'}}>
-                <Stack><img className='white-logo' src="/assets/Image/logo.svg" alt="" /></Stack>
+            <Stack direction={'row'} padding={'12px'} justifyContent={"space-between"} zIndex={2} sx={{ backgroundColor: 'transparent', position:"absolute" , top:"0",left:"0",right:"0"}}>
+                <Stack mx={3}><img className='white-logo' src="/assets/Image/logo.svg" alt="" /></Stack>
                 <Stack direction={'row'} spacing={3}>
                     <Button color='inherit' sx={{ fontSize: '12px', display: { md: 'block', xs: 'none' } }}> سوالات متداول</Button>
                     <Button color='inherit' sx={{ fontSize: '12px', display: { md: 'block', xs: 'none' } }}>دانلود اپلیکیشن</Button>
--- frontend/src/Pages/Home/Index.jsx
+++ frontend/src/Pages/Home/Index.jsx
@@ -1,10 +1,71 @@
-import { Stack } from '@mui/material';
-import React from 'react';
+import { InputAdornment, Stack, TextField, Typography, Button } from '@mui/material';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import LockIcon from '@mui/icons-material/Lock';
+import React, { useRef } from 'react';
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import './style.css'
+
+
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const Home = () => {
+    const greenContainer = useRef()
+    const meal = useRef()
+    const tl = gsap.timeline()
+    const loginForm = useRef();
+    useGSAP(() => {
+        tl.fromTo(loginForm.current, { opacity: 0 }, { opacity: 1, delay: "1" })
+        tl.to(greenContainer.current, { x: "-=35%", delay: "0.5" })
+        tl.to(meal.current, { x: "-=135%", delay: "0.5", rotate: "360" })
+
+
+    }, [])
+
+
     return (
-        <Stack height={'100vh'}>
-            
+        <Stack height={"100vh"} flexDirection={"row"} position={'relative'} overflow={"hidden"} sx={{ backgroundImage: "url('assets/image/bg.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "cover", }} >
+            <Stack ref={greenContainer} width={"100%"} height={"100vh"} position={"absolute"} bgcolor={'primary.main'} sx={{ right:"100%", boxShadow: '5px 0px 50px 5px' }}></Stack>
+            <Stack ref={meal} height={"100vh"} sx={{ position: 'absolute', right: {xl:"100%",lg:"110%",md:"145%",xs:"170%"} }} justifyContent={"center"}>
+                <img className='plate' src='/assets/image/meal.png' />
+
+            </Stack>
+            <Stack justifyContent={"center"} alignItems={"flex-end"} sx={{ margin: "0 auto", width: "100%", maxWidth: { lg: "1185px", xs: "100%" } }} px={2} >
+                <Stack ref={loginForm} width={{sm:"450px",xs:"100%"}} height={{sm:"500px",xs:"auto"}} gap={3} sx={{ backgroundColor: {md:"rgba(0, 0 ,0 ,0.1)",xs:"rgba(255,255,255, 0.9)"}, zIndex: "4" }} alignItems={"center"} justifyContent={"center"} px={{sm:"32px",xs:"16px"}} py={2}>
+                    <Typography textAlign={"center"} variant='h1' color={"#000"} fontSize={"30px"}> از مزرعه تا آشپزخانه</Typography>
+                    <Typography textAlign={"center"} variant='h1' color={"#000"} fontSize={"21px"}> برای سفارش از <span className='login-span'> تازه بار</span> ثبت نام کنید</Typography>
+                    <Typography textAlign={"center"} color={"#000"} > خدمات سریع و هوشمند ارسال بار تازه از بهترین کشاورزان به راحتی و با چند کلیک مواد تازه و با کیفیت مورد نیاز خود را از تازه بار دریافت کنید </Typography>
+                    <TextField
+                        placeholder='09xxxxxxxxx'
+                        dir='ltr'
+
+                        sx={{ backgroundColor: "#fff", alignSelf: "stretch" }}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <PhoneAndroidIcon sx={{color:"primary.main"}}/>
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <TextField
+                        placeholder='رمز عبور خود را وارد کنید '
+                        sx={{ backgroundColor: "#fff", alignSelf: "stretch" }}
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment position="start">
+                                    <LockIcon sx={{color:"primary.main"}}/>
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                    <Button variant="contained" sx={{ backgroundColor: "#000", alignSelf: "stretch", lineHeight: "45px", fontSize: "17px" }} >ورود به تازه بار</Button>
+                </Stack>
+
+            </Stack>
+
         </Stack>
     );
 }
