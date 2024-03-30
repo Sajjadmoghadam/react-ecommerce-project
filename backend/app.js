f9226fca8e011dbd5b8762ec5be0fd04be245309
Sajjadmoghadam <sajjad.moghadam1379@gmail.com>
2024-03-30 19:59:34 +0330

    create folder structure of backend

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import morgan from 'morgan'
+
+const app = express()
+app.use(morgan('dev')) 
+
+
+
+
+
+
+
+
+
+
+export default app
---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import app from "./app.js";
+import { config } from "dotenv";
+config()
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("SERVER RUN");
+})
