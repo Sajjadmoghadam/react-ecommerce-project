03c2e48ed6ea041e4b8a5e386cbcd4d496a99a8a
Sajjadmoghadam <sajjad.moghadam1379@gmail.com>
2024-03-30 14:56:31 +0330

    create home section two

--- frontend/src/Pages/Home/Index.jsx
+++ frontend/src/Pages/Home/Index.jsx
@@ -1,4 +1,4 @@
-import { InputAdornment, Stack, TextField, Typography, Button, Divider } from '@mui/material';
+import { InputAdornment, Stack, TextField, Typography, Button, Divider, Chip } from '@mui/material';
 import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
 import LockIcon from '@mui/icons-material/Lock';
 import React, { useRef } from 'react';
@@ -26,6 +26,8 @@ const Home = () => {
 
 
     }, [])
+
+    
 
 
     return (
@@ -72,14 +74,32 @@ const Home = () => {
 
             </Stack>
             <Stack width={"100%"} py={6}>
-                <Stack width={{ lg: "1185px", md: "850px", xs: "100%" }} sx={{ margin: "0 auto" }} gap={2} >
+                <Stack width={{ lg: "1185px", md: "850px", xs: "100%" }} sx={{ margin: "0 auto" }} gap={2} px={2}>
                     <Typography textAlign={"center"} variant='h2' fontSize={"30px"}> تعدادی از مشتریان <span className='login-span'>تازه بار</span></Typography>
                     <Divider variant='middle' sx={{ color: "red" }} />
                     <Typography textAlign={"center"} variant='h3' fontSize={"16px"}>روزانه محصولات تازه بار را برای تهیه صبحانه، ناهار، شام و پیش غذا انتخاب می‌کنند</Typography>
                     <Stack overflow={'hidden'} py={2}>
-                       <Customers/>
+                        <Customers />
                     </Stack>
 
+                </Stack>
+            </Stack>
+            <Stack width={"100%"} py={6}>
+                <Stack width={{ lg: "1185px", md: "850px", xs: "100%" }} sx={{ margin: "0 auto" }} gap={3}  px={2}>
+                    <Typography fontSize={"21px"} textAlign={"center"} variant='h2'><span className='login-span'>تازه بار</span> ، پیشگام سفارش آنلاین بار تازه در ایران</Typography>
+                    <Stack flexDirection={"row"} flexWrap={'wrap'} gap={1} justifyContent={"center"}>
+                        <Chip label="رستوران"/>
+                        <Chip label="فست فود"/>
+                        <Chip label="آشپزخانه"/>
+                        <Chip label="کافی شاپ"/>
+                        <Chip label="تالار"/>
+                        <Chip label="هتل"/>
+                        <Chip label="بیمارستان"/>
+                        <Chip label="مدارس"/>
+                        <Chip label="مساجد"/>
+                        <Chip label="هایپر مارکت"/>
+                        <Chip label="آبمیوه فروشی"/>
+                    </Stack>
                 </Stack>
             </Stack>
         </>
